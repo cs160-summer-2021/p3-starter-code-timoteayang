a0df517bac38b8aed20317ac30e801a83dac63ed
Timothy Yang <timoteayang@berkeley.edu>
2021-07-14 22:47:35 -0700

    finish proj3

--- coloring/urls.py
+++ coloring/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('demo', views.index, name='demo'),
-    path('new_interaction', views.index, name='new_interaction')
+    path('', views.index, name='index'),
+    path('demo', views.demo, name='demo'),
+    path('new_interaction', views.index, name='new_interaction'),
+    path('home', views.home, name='home'),
+    path('artboard', views.artboard, name='artboard'),
+    path('login', views.index, name='index'),
+    path('signup', views.signup, name='signup'),
+    path('profile', views.profile, name='profile'),
+    path('clashes', views.clashes, name='clashes'),
+    path('leaderboard', views.leaderboard, name='leaderboard'),
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
 
+def demo(request):
+    return render(request, 'coloring/demo.html')
+
 def index(request):
     return render(request, 'coloring/index.html')
+
+def home(request):
+    return render(request, 'coloring/home.html')
+
+def artboard(request):
+    return render(request, 'coloring/artboard.html')
+
+def login(request):
+    return render(request, 'coloring/index.html')
+
+def signup(request):
+    return render(request, 'coloring/signup.html')
+
+def profile(request):
+    return render(request, 'coloring/profile.html')
+
+def clashes(request):
+    return render(request, 'coloring/clashes.html')
+
+def leaderboard(request):
+    return render(request, 'coloring/leaderboard.html') 
